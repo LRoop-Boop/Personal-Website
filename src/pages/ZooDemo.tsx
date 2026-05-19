f671c3a967e6878315357a1741914756a3a8df90
Lucy Roop <larghmi@gmail.com>
2026-05-19 08:21:51 -0400

    Add Vercel routing config

--- src/pages/ZooDemo.tsx
+++
@@ -1,8 +0,0 @@
-export default function ZooDemo(){
-    return(
-        <div className="min-h-screen bg-slate-100 text-slate-900 px-6 py-16">
-            <h1 className="text-3xl font-bold leading-tight">Welcome to the Zoo</h1>
-            
-        </div>
-    )
-}
